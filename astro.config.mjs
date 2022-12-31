f95fd5f6c1d53e38dfb21b8877e2e64d23a3530c
eddie cianci <defeated2k4@gmail.com>
2022-12-31 12:40:40 -0500

    new year new domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // https://astro.build/config
 
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 
 export default defineConfig({
+  site: "https://eddiecianci.info",
   integrations: [tailwind(), vue(), sitemap(), image()]
 });
