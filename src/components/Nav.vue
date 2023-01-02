e6d3d9bf17388e43cc43065d2da0a9cfd49d9dbc
eddie cianci <defeated2k4@gmail.com>
2023-01-01 22:12:34 -0500

    tweak light mode colors

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
   <menu
-    class="flex rounded-full px-3 text-sm font-medium shadow-lg shadow-zinc-800/5 backdrop-blur bg-white/90 dark:bg-zinc-800/90 text-zinc-800 dark:text-zinc-200 ring-1 ring-zinc-900/5 dark:ring-white/10">
+    class="flex rounded-full px-3 text-sm font-medium shadow-lg shadow-zinc-800/5 backdrop-blur bg-zinc-50 dark:bg-zinc-800/90 text-zinc-800 dark:text-zinc-200 ring-1 ring-zinc-900/5 dark:ring-white/10">
 
     <NavLink v-for="link in links" :current="current" :link="link" />
 
