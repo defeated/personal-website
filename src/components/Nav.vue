43591eae1a9dc22b2e6bb1671f01d2c566b49a70
eddie cianci <defeated2k4@gmail.com>
2023-01-01 21:53:10 -0500

    extract NavLink component, improve isActive

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,28 +2,17 @@
   <menu
     class="flex rounded-full px-3 text-sm font-medium shadow-lg shadow-zinc-800/5 backdrop-blur bg-white/90 dark:bg-zinc-800/90 text-zinc-800 dark:text-zinc-200 ring-1 ring-zinc-900/5 dark:ring-white/10">
 
-    <li v-for="link in links">
-      <a :href="link.path" :class="{ 'text-fuchsia-400': isActive(link) }"
-        class="relative block px-3 py-2.5 transition hover:text-fuchsia-400">
-
-        {{ link.text }}
-
-        <span v-if="isActive(link)"
-          class="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-fuchsia-400/0 via-fuchsia-400/60 to-fuchsia-400/0"></span>
-      </a>
-    </li>
+    <NavLink v-for="link in links" :current="current" :link="link" />
 
   </menu>
 </template>
 
 <script lang="ts" setup>
-const props = defineProps({
-  current: String
-})
+import NavLink from "./NavLink.vue";
 
-function isActive(link: any) {
-  return link.path === props.current
-}
+const props = defineProps<{
+  current: string
+}>()
 
 const links = [{
   text: "About",
@@ -31,7 +20,7 @@ const links = [{
 },
 {
   text: "Writing",
-  path: "/writing",
+  path: "/blog",
 },
 {
   text: "Projects",
---
+++ src/components/NavLink.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    <a :href="link.path" :class="{ 'text-fuchsia-400': isActive }"
+      class="relative block px-3 py-2.5 transition hover:text-fuchsia-400">
+
+      {{ link.text }}
+
+      <span v-if="isActive"
+        class="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-fuchsia-400/0 via-fuchsia-400/60 to-fuchsia-400/0"></span>
+    </a>
+  </li>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  current: String,
+  link: { path: string, text: string },
+}>()
+
+const isActive = props.current === props.link.path || props.current.startsWith(props.link.path + "/")
+</script>
