d15e021df81385ff3b0150db5220aca99d2959bc
eddie cianci <defeated2k4@gmail.com>
2023-01-15 19:02:52 -0500

    update /blog to sort by pubDate

--- src/components/BlogArchive.vue
+++ src/components/BlogArchive.vue
@@ -1,21 +1,21 @@
 <template>
   <section class="mt-16 sm:mt-20 max-w-3xl">
 
-    <article v-for="post in posts" class="flex flex-col lg:flex-row items-baseline">
+    <article v-for="post in posts" class="flex flex-col lg:flex-row items-baseline pb-14">
       <time :datetime="pubDateTime(post)"
         class="mb-3 lg:w-1/3 text-sm text-zinc-500 dark:text-zinc-400/80 border-l dark:border-zinc-700 border-zinc-200 pl-3 lg:pl-6">
-        {{ post.frontmatter.pubDate }}
+        {{ post.frontmatter?.pubDate }}
       </time>
 
       <a :href="post.url"
         class="flex-1 rounded-md transition hover:lg:ring-[14px] hover:lg:ring-offset-[14px] hover:lg:ring-offset-transparent hover:lg:bg-zinc-100 hover:lg:ring-zinc-100 hover:lg:dark:bg-zinc-800/50 hover:lg:dark:ring-zinc-800/50">
 
         <h2 class="font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
-          {{ post.frontmatter.title }}
+          {{ post.frontmatter?.title }}
         </h2>
 
         <p class="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-          {{ post.frontmatter.description }}
+          {{ post.frontmatter?.description }}
         </p>
 
         <span
@@ -30,10 +30,10 @@
 
 <script lang="ts" setup>
 const props = defineProps<{
-  posts: { url?: string, frontmatter: { title?: string, description?: string, pubDate?: Date } }[]
+  posts: { url?: string, frontmatter?: { title?: string, description?: string, pubDate?: Date } }[]
 }>()
 
 function pubDateTime(post: any) {
-  return new Date(post.frontmatter.pubDate).toISOString()
+  return new Date(post.frontmatter?.pubDate).toISOString()
 }
 </script>
