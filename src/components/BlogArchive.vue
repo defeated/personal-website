4a7207494f6475d6aef60e5bd2bb9cf8a0a06495
eddie cianci <defeated2k4@gmail.com>
2023-01-16 12:42:36 -0500

    add Draft badge to /blog

--- src/components/BlogArchive.vue
+++ src/components/BlogArchive.vue
@@ -6,6 +6,11 @@
       <time :datetime="pubDateTime(post)"
         class="mb-3 lg:w-1/3 text-sm text-zinc-500 dark:text-zinc-400/80 lg:border-0 border-l dark:border-zinc-700 border-zinc-200 pl-3 lg:pl-6">
         {{ post.frontmatter?.pubDate }}
+    
+        <span v-if="isDraft(post)"
+          class="ml-1 inline-flex items-center rounded bg-yellow-100 px-2 py-0.5 text-xs font-medium text-yellow-800">
+          Draft
+        </span>
       </time>
 
       <a :href="post.url"
@@ -36,5 +41,9 @@ const props = defineProps<{
 
 function pubDateTime(post: any) {
   return new Date(post.frontmatter?.pubDate).toISOString()
+}
+
+function isDraft(post: any) {
+  return Boolean(post.frontmatter?.draft)
 }
 </script>
