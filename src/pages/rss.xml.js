061350f8707f88275d92e5a82331f995f8014b15
eddie cianci <defeated2k4@gmail.com>
2023-01-22 18:44:00 -0500

    feat(rss): enable rss feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,20 @@
+import rss from "@astrojs/rss";
+
+export function get(context) {
+  return rss({
+
+    // `<title>` field in output xml
+    title: "Eddie Cianci - Writing about people, process and technology.",
+
+    // `<description>` field in output xml
+    description: "Thoughts on a career building software and software teams. From playing senior roles on 'both ends of the horse' (frontend & backend), to mentoring and coaching engineers, to managing a portfolio of products & teams.",
+
+    // Pull in your project "site" from the endpoint context
+    // https://docs.astro.build/en/reference/api-reference/#contextsite
+    site: context.site.toString(),
+
+    // Array of `<item>`s in output xml
+    // See "Generating items" section for examples using content collections and glob imports
+    items: import.meta.glob("./blog/*.{md,mdx}")
+  });
+}
